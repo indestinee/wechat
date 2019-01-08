8cd2113c3424adff4d8f25b39ba58c1bb489cdc5
indestinee <indestinee@gmail.com>
2019-01-08 20:05:02 +0800

    update

--- db_op.py
+++ db_op.py
@@ -8,6 +8,22 @@ with open('./data/config.json') as f:
 def set_admin():
     for admin in _cfg_['admins']:
         db.upd_row('user', limitation={'wechat': admin}, data={'level': 100})
+
+def check_user():
+    users = db.select('user', limitation={'level': 0})
+    for user in users:
+        print(user)
+        n_level = input('new level: ')
+        n_level = int(n_level)
+        assert n_level >= 0 and n_level <= 100
+        db.upd_row('user', limitation=user, data={'level': n_level})
+
 if __name__ == '__main__':
-    set_admin()
+    # set_admin()
+
+    check_user()
+
+
+    pass
+    
 
